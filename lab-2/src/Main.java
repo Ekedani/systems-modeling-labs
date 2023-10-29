abb1005d290c0b3544d213cfb7134d53b07c1020
Vladlen <ilusenkovladlen@gmail.com>
2023-10-29 13:24:34 +0200

    copied code from lectures

--- lab-2/src/Main.java
+++ lab-2/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lab 2");
+        Create c = new Create(2.0);
+        Process p = new Process(1.0);
+        System.out.println("id0 = " + c.getId() + " id1=" + p.getId());
+        c.setNextElement(p);
+        p.setMaxqueue(5);
+        c.setName("CREATOR");
+        p.setName("PROCESSOR");
+        c.setDistribution("exp");
+        p.setDistribution("exp");
+        ArrayList<Element> list = new ArrayList<>();
+        list.add(c);
+        list.add(p);
+        Model model = new Model(list);
+        model.simulate(1000.0);
     }
 }
